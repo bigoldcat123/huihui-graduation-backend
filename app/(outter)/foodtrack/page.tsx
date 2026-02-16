3ce488a3d28278efd4777e93aed1d8bbe71b5014
Dale Lowe <dadigua@Dales-MacBook-Pro.local>
2026-02-16 12:00:24 +0800

    Add food edit dialog and avatar

---
+++ app/(outter)/foodtrack/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+export default function FoodTrackPage() {
+    return (
+        <div className=" min-h-screen bg-[url(/bg.png)] bg-cover bg-center bg-fixed" >
+            
+        </div>
+    )
+}
